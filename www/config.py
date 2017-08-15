8a745011e7da0e8879802a831cbd36214ecbf802
xinzhigit <dongxinzhiemail@163.com>
2017-08-15 23:36:50 +0800

    添加处理文件

---
+++ www/config.py
@@ -0,0 +1,53 @@
+'''
+Configuration
+'''
+
+import config_default
+
+class Dict(dict):
+    '''
+    Simple dict but support access as x.y style
+    '''
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k,v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+# 用override的字段中的字段替换defaults字段中的字段
+def merge(defaults, override):
+    r = {}
+    for k,v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+# 将dict转换为Dict
+def toDict(d):
+    D = Dict()
+    for k,v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
+
+configs = config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations
+'''
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'xinzhi',
+        'password':'xinzhi123',
+        'db':'awesome'
+    },
+    'session':{
+        'secret':'Awesome'
+    }
+}
